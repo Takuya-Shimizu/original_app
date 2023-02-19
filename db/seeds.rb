a550378390d7297c3d2665092a955bcfa588f7e1
takuya shimizu <p.t.901.ts@gmail.com>
2023-02-20 07:20:56 +0900

    ジャンルとラベルのデータを登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
-users = ["yamada", "abe", "tanaka", "yabe", "kimura"]
-users.each_with_index do |user, i|
-  User.create(
-    name: "#{user}",
-    email: "#{i + 1}@gmail.com",
-    password: "password"
-  )
+# users = ["yamada", "abe", "tanaka", "yabe", "kimura"]
+# users.each_with_index do |user, i|
+#   User.create(
+#     name: "#{user}",
+#     email: "#{i + 1}@gmail.com",
+#     password: "password"
+#   )
+# end
+
+require "csv"
+ 
+CSV.foreach('db/genre.csv') do |info|
+  Genre.create(:name => info[0])
+end
+
+CSV.foreach('db/label.csv') do |info|
+  Label.create(:name => info[0])
 end
