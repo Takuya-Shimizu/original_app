b358a182f36936ae1ba09dd3ebde112be0d99b25
takuya shimizu <p.t.901.ts@gmail.com>
2023-02-20 13:05:32 +0900

    管理画面の実装

--- db/migrate/20230216052357_create_comments.rb
+++ db/migrate/20230216052357_create_comments.rb
@@ -1,7 +1,6 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-      t.string :title, null: false
       t.text :content, null: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2023_02_19_231028) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "title", null: false
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
