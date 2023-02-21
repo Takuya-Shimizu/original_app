5a217a4e3274b5b8acf62785a69309e9e9889541
takuya shimizu <p.t.901.ts@gmail.com>
2023-02-21 15:48:48 +0900

    ゲストログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    post 'users/admin_guest_sign_in', to: 'users/sessions#admin_guest_sign_in'
+  end
+
   resources :favorites, only: [:create, :destroy]
+  
   resources :posts do
     resources :comments
   end
+
   root 'welcome#index'
+  
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
