99bfe7e1f0103ba61464d6e93c6ee1da422f9b0e
takuya shimizu <p.t.901.ts@gmail.com>
2023-02-26 23:56:30 +0900

    seedデータ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ end
 5.times do |i|
   post = Post.create(
     title: "test_title#{i + 1}",
-    content: "#test_content#{i + 1}",
+    content: "test_content#{i + 1}",
     user_id: "#{i + 1}",
   )
 
@@ -30,8 +30,11 @@ end
   )
 end
 
-post = Post.create(title: 'test990', content: 'test990')
+# post = Post.create(title: 'test990', content: 'test990')
 Comment.create([
-  { post_id: '1', user_id: '2', content: 'test_comment'},
-  { post_id: '3', user_id: '4', content: 'test_comment'},
+  { post_id: '1', user_id: '2', content: 'test_comment_by_user_id_2'},
+  { post_id: '2', user_id: '3', content: 'test_comment_by_user_id_3'},
+  { post_id: '3', user_id: '4', content: 'test_comment_by_user_id_4'},
+  { post_id: '4', user_id: '5', content: 'test_comment_by_user_id_5'},
+  { post_id: '5', user_id: '1', content: 'test_comment_by_user_id_1'},
 ])
