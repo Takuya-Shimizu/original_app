e2bcd5363fb435809a39a0cf0ea8d92cf5b88c06
takuya shimizu <p.t.901.ts@gmail.com>
2023-02-27 18:24:33 +0900

    本番環境管理画面ログアウト不具合修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap';
-import '../stylesheets/application';
 import '../stylesheets/top';
 import '../stylesheets/registrations_new';
 import '../stylesheets/post_form';
