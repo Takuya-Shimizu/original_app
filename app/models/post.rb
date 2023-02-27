feaa8a6cbcc260df614345cbe62df40ccfd99e63
takuya shimizu <p.t.901.ts@gmail.com>
2023-02-27 23:08:02 +0900

    バリデーション追加 & レイアウト修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 400 }
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
