2f034351469ee9c01c042c6634ad00fb521e5fc2
takuya shimizu <p.t.901.ts@gmail.com>
2023-02-27 19:49:33 +0900

    デザイン修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import '../stylesheets/top';
-import '../stylesheets/registrations_new';
-import '../stylesheets/post_form';
+
 
 Rails.start()
 Turbolinks.start()
